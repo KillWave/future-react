98cf2ffa4edec65aeba1f43e643fb493afa487d8
KillWave <1353207933@qq.com>
2020-10-18 17:24:20 +0800

    update

--- src/element-engin.ts
+++ src/element-engin.ts
@@ -1,7 +1,7 @@
 
 import { render } from './html-engin/render'
 import { TemplateResult } from './html-engin/template-result'
-export { html } from './html-engin'
+export { html,svg } from './html-engin'
 export  abstract class ElementEngin extends HTMLElement {
   public readonly root: ShadowRoot = this.attachShadow({ mode: 'closed' });
   abstract render(): TemplateResult;
@@ -17,19 +17,19 @@ export  abstract class ElementEngin extends HTMLElement {
     render(this.render(), this.root);
   }
   private subscribe() {
-    var observer = new MutationObserver(function (mutations, observer) {
-      mutations.forEach(function (mutation) {
-        console.log(mutation);
-      });
-    });
-    observer.observe(this.root, {
-      attributes: true,
-      characterData: true,
-      childList: true,
-      subtree: true,
-      attributeOldValue: true,
-      characterDataOldValue: true
-    })
+    // var observer = new MutationObserver(function (mutations, observer) {
+    //   mutations.forEach(function (mutation) {
+    //     console.log(mutation);
+    //   });
+    // });
+    // observer.observe(this.root, {
+    //   attributes: true,
+    //   characterData: true,
+    //   childList: true,
+    //   subtree: true,
+    //   attributeOldValue: true,
+    //   characterDataOldValue: true
+    // })
 
     for (const key in this) {
       if (Object.prototype.hasOwnProperty.call(this, key)) {
