44c6786dc24f6cc3d90bd0545a82966e6177112d
KillWave <1353207933@qq.com>
2020-10-18 16:52:05 +0800

    deleted:    dist/bundle.js 	deleted:    dist/bundle.js.map 	deleted:    dist/element-engin.d.ts 	deleted:    dist/html-engin/default-template-processor.d.ts 	deleted:    dist/html-engin/directive.d.ts 	deleted:    dist/html-engin/dom.d.ts 	deleted:    dist/html-engin/index.d.ts 	deleted:    dist/html-engin/interface/part.d.ts 	deleted:    dist/html-engin/interface/render-options.d.ts 	deleted:    dist/html-engin/interface/template-processor.d.ts 	deleted:    dist/html-engin/modify-template.d.ts 	deleted:    dist/html-engin/parts.d.ts 	deleted:    dist/html-engin/render.d.ts 	deleted:    dist/html-engin/template-cache.d.ts 	deleted:    dist/html-engin/template-instance.d.ts 	deleted:    dist/html-engin/template-result.d.ts 	deleted:    dist/html-engin/template.d.ts 	deleted:    dist/index.html 	deleted:    dist/main.d.ts 	deleted:    package-lock.json

--- dist/html-engin/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { SVGTemplateResult, TemplateResult } from './template-result';
-export declare const html: (strings: TemplateStringsArray, ...values: unknown[]) => TemplateResult;
-/**
- * Interprets a template literal as an SVG template that can efficiently
- * render to and update a container.
- */
-export declare const svg: (strings: TemplateStringsArray, ...values: unknown[]) => SVGTemplateResult;
