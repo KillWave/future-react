0f142173668b3e3f722ff6701859b4efefa3863f
刘浪 <e-lang.liu@geely.com>
2021-06-18 13:13:31 +0800

    feat: add dev

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,12 @@ import resolve from 'rollup-plugin-node-resolve';
 import htmlTemplate from 'rollup-plugin-generate-html-template';
 import serve from 'rollup-plugin-serve'
 import json from '@rollup/plugin-json'
-import typescript from 'rollup-plugin-typescript2';
+// import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs'
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
-// import jsx from 'rollup-plugin-jsx'
+// import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import babel from 'rollup-plugin-babel';
+import jsx from 'rollup-plugin-jsx'
+// const extensions = ['.js', '.ts', 'tsx'];
 export default {
     input: 'src/main.tsx',
     output: {
@@ -21,12 +23,16 @@ export default {
         resolve({
             extensions: ['.tsx', '.js', '.ts', '.json'],
         }),
-
-        typescript(),
-        // jsx({ factory: 'React.component' }),
-        getBabelOutputPlugin({
-            plugins: ["@babel/plugin-external-helpers"]
+        babel({
+            exclude: 'node_modules/**',
+            extensions: ['.tsx', '.js', '.ts', '.json'],
         }),
+
+        jsx({ factory: 'React.component' }),
+        // typescript(),
+        // getBabelOutputPlugin({
+        //     plugins: ["@babel/plugin-external-helpers"]
+        // }),
 
         htmlTemplate({
             template: 'example/demo.html',
